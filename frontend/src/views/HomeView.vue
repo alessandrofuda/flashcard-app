938dfe5093a4c49ae058976d30040d7dc549f3d6
alessandrofuda <alessandro.fuda@gmail.com>
2023-10-11 18:43:05 +0200

    installed Balm UI

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+
+    <div class="hello">
+      https://material.balmjs.com/guide/quick-start <br>
+      BalmUI TEST:
+      <!-- Add a test button -->
+      <ui-button icon="add" @click="$alert('Hello BalmUI')">Click Me</ui-button>
+    </div>
+
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+    <TheWelcome />
+  </main>
+</template>
